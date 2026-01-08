ea6161e02be948d6ff9ef30d430b103e8a348971
Kevin Chen <108315438+kevinkchen1@users.noreply.github.com>
2026-01-07 18:53:50 -0800

    mayo web scraping

---
+++ scrape.py
@@ -0,0 +1,8 @@
+supplements = [
+    "acidophilus","aloe","coenzyme-q10","creatine","dhea",
+    "evening-primrose","fish-oil","flaxseed-and-flaxseed-oil",
+    "folate-folic-acid","ginkgo","glucosamine","honey","l-arginine",
+    "marijuana","melatonin","milk-thistle","niacin","red-yeast-rice",
+    "same","st-johns-wort","tea-tree-oil","vitamin-a","vitamin-b-6",
+    "vitamin-b-12","vitamin-c","vitamin-d","vitamin-e","zinc"
+]
