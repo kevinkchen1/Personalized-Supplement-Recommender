78e25bc15d46be624ed07d44d8657101b41c2344
Kevin Chen <108315438+kevinkchen1@users.noreply.github.com>
2026-01-08 23:44:50 -0800

    rough draft of symptoms table

--- data/scrape.py
+++
@@ -1,8 +0,0 @@
-supplements = [
-    "acidophilus","aloe","coenzyme-q10","creatine","dhea",
-    "evening-primrose","fish-oil","flaxseed-and-flaxseed-oil",
-    "folate-folic-acid","ginkgo","glucosamine","honey","l-arginine",
-    "marijuana","melatonin","milk-thistle","niacin","red-yeast-rice",
-    "same","st-johns-wort","tea-tree-oil","vitamin-a","vitamin-b-6",
-    "vitamin-b-12","vitamin-c","vitamin-d","vitamin-e","zinc"
-]
